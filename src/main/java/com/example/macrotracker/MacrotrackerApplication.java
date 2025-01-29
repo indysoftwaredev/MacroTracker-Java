6a059a9eb870cda7f39b43cd6ccb395066fa5c67
David Carter <indysoftwaredev@gmail.com>
2025-01-29 13:57:30 -0500

    Initial commit

---
+++ src/main/java/com/example/macrotracker/MacrotrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.macrotracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MacrotrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MacrotrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/macrotracker/MacrotrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.macrotracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MacrotrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
